1c7798f6217e83344960799a33868a581c2277d6
kkojimawk <kkojimawk@gmail.com>
2023-04-10 16:58:32 +0900

    add auth login

--- server.js
+++ server.js
@@ -18,9 +18,14 @@ mongoose
   });
 
 //ミドルウェア
+app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
