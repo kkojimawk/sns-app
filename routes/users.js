5bca1892e0f2cb5ad30cb34788ac4946f9db7b2d
kkojimawk <kkojimawk@gmail.com>
2023-04-10 12:40:40 +0900

    register add err

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("user router");
+//ユーザー登録
+router.post("/register", (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
+
+// router.get("/", (req, res) => {
+//   res.send("user router");
+// });
 
 module.exports = router;
