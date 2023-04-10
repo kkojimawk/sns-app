bff15ce62efa43363caea7762edb5107a4c31b38
kkojimawk <kkojimawk@gmail.com>
2023-04-10 17:36:04 +0900

    add user CRUD

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,76 @@
+const User = require("../models/User");
+
 const router = require("express").Router();
 
-//ユーザー登録
-router.post("/register", (req, res) => {
-  try {
-  } catch (err) {
-    return res.status(500).json(err);
+//CRUD
+//ユーザー情報の更新
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      return res.status(200).json("アカウント情報を更新しました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("自分のアカウントのみ編集できます");
   }
 });
 
-// router.get("/", (req, res) => {
-//   res.send("user router");
-// });
+//ユーザー情報の削除
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      return res.status(200).json("アカウントを削除しました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("自分のアカウントのみ削除できます");
+  }
+});
+
+//ユーザー情報の取得
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      //フォローしていない場合
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+      } else {
+        return res.status(403).json("すでにフォローしています");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("自分をフォローすることはできません");
+  }
+});
 
 module.exports = router;
