e4f136ea7a665bc85a9432c442b1832a5d5dd198
kkojimawk <kkojimawk@gmail.com>
2023-04-10 17:54:10 +0900

    add post

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,15 @@
+const Posts = require("../models/Posts");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("posts router");
+//投稿を作成する
+router.post("/", async (req, res) => {
+  const newPost = new Posts(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
