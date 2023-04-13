d99e439358429ba7a49c08ded4c73c563bfbf58c
kkojimawk <kkojimawk@gmail.com>
2023-04-13 15:00:16 +0900

    投稿作成API、投稿編集API作成

--- routes/posts.ts
+++ routes/posts.ts
@@ -8,9 +8,24 @@ router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
+    return res.status(200).json(savedPost);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
+  }
+});
+
+//投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post?.userId === req.body.userId) {
+      await post?.updateOne({ $set: req.body });
+      return res.status(200).json("投稿を更新しました");
+    } else {
+      return res.status(403).json("投稿を編集する権限がありません");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
   }
 });
 
